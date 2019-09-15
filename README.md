1d86419f1948b14b1d91d4bf076a3cab8d0b8b3e
ranguangyu <593289488@qq.com>
2019-09-15 20:43:10 +0800

    router

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # ky-react-exam
-exam for fe
+
+页面组件按需加载 + 热更新 + 热替换 + mobx-react
+
+## Project setup
+```
+npm install --verbose
+```
+
+## Development
+```
+npm run dev
+
+http://127.0.0.1:9999/
+```
+
+## Build
+```
+npm run prod
+```
--- app/components/Systems/RootRouter/index.jsx
+++ app/components/Systems/RootRouter/index.jsx
@@ -1,17 +1,31 @@
 /**
  * 根路由组件
  * @author ranguangyu
- * @date 2019-2-2
+ * @date 2019-8-9
  */
 
 import React from "react";
-import AnimateRouter from "@components/Systems/AnimateRouter";
+import { Switch, Route, Redirect, withRouter } from "react-router-dom";
+import routes from "@components/Systems/router";
+import NotFound from "@components/Systems/NotFound";
 
+@withRouter
 class RootRouter extends React.Component {
   render() {
+    const location = this.props.location;
     return (
-      <div style={{padding: 15}}>
-        <AnimateRouter />
+      <div>
+        <div style={{padding: 15}}>
+          <Switch location={location}>
+            {routes.map(({ name, path, exact = true, component }) => {
+              return (
+                <Route key={name} path={path} exact={exact} component={component} />
+              )
+            })}
+            {/* <Redirect from="/" exact to="/home" /> */}
+            <Route path="*" component={NotFound} />
+          </Switch>
+        </div>
       </div>
     )
   }
